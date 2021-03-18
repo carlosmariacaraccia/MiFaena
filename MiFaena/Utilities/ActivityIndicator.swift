951e6cbe95c5489a1df3e7f111a2404a4d179e03
Carlos Maria Caraccia <carloscaraccia@gmail.com>
2021-03-18 11:08:11 -0300

    Added utilities.

---
+++ MiFaena/Utilities/ActivityIndicator.swift
@@ -0,0 +1,26 @@
+//
+//  ActivityIndicator.swift
+//  MeatTrade
+//
+//  Created by Carlos Caraccia on 2/5/21.
+//
+
+import UIKit
+
+
+class SpinnerViewController: UIViewController {
+    var spinner = UIActivityIndicatorView(style: .large)
+
+    override func loadView() {
+        view = UIView()
+        view.backgroundColor = UIColor(white: 0, alpha: 0.7)
+
+        spinner.translatesAutoresizingMaskIntoConstraints = false
+        spinner.startAnimating()
+        spinner.color = .white
+        view.addSubview(spinner)
+
+        spinner.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        spinner.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+    }
+}
---
+++ MiFaena/Utilities/Utils.swift
@@ -0,0 +1,8 @@
+//
+//  Utils.swift
+//  MiFaena
+//
+//  Created by Carlos Caraccia on 3/18/21.
+//
+
+import Foundation
